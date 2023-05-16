da1e94ca3ffa3446b112236cbf99fac076887c42
jeje <jessicamosby12@gmail.com>
2023-05-16 19:06:35 -0300

    environments

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,32 +1,55 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
-import { FormGroup } from '@angular/forms';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
+
+import { Moment } from 'src/app/Moments';
 
 @Component({
   selector: 'app-moment-form',
   templateUrl: './moment-form.component.html',
   styleUrls: ['./moment-form.component.css']
 })
-export class MomentFormComponent implements OnInit {
- 
-@Input() btnText!: string;
 
-momentForm!: FormGroup
+export class MomentFormComponent implements OnInit {
+
+  @Output() onSubmit = new EventEmitter<Moment>()
+  @Input() btnText!: string;
+         momentForm!: FormGroup;
 
   constructor(){}
 
   ngOnInit(): void {
     this.momentForm = new FormGroup({
       id: new FormControl(''),
-      title: new FormControl(''),
-      description: new FormControl(''),
-      image: new FormControl('')
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]), 
+      image: new FormControl('', [Validators.required])
       
     });
   }
 
-  submit() {
-    console.log("enviar formulario");
+  get title() {
+    return this.momentForm.get('title')!;
   }
+
+  get description() {
+    return this.momentForm.get('description')!;
+  }
+
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+    this.momentForm.patchValue({ image: file });
+  };
+
+  submit() {
+   
+    if(this.momentForm.invalid) {
+      return;
+    }
+    console.log(this.momentForm.value);
+   
+    this.onSubmit.emit(this.momentForm.value); 
+  }
+
   
 }
--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Moment } from 'src/app/Moments';
+import { MomentService } from 'src/app/services/moment.service';
+
 @Component({
   selector: 'app-new-moment',
   templateUrl: './new-moment.component.html',
   styleUrls: ['./new-moment.component.css']
 })
 export class NewMomentComponent implements OnInit {
+   btnText = 'Compartilhar!';
 
-  constructor() {}
+
+  constructor(private momentService: MomentService) {}
   
   ngOnInit(): void {}
+
+    async createHandler(moment: Moment){ 
+      const formData = new FormData();
+
+      formData.append('title', moment.title);
+      formData.append('description',moment.description);
+
+      if (moment.image) {
+         formData.append('image', moment.image);
+      }
+
+      await this.momentService.createMoment(formData).subscribe();
+    }
+
 
 
 }
